8fce00a0568fd74d296b55da989b1aaffccccfd7
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-04-06 23:08:34 +0500

    UI done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-
+import React from 'react'
+import { TextField, Typography } from '@material-ui/core';
 import './App.css';
+import BMICard from './Components/BMICard';
 
 function App() {
   return (
-    <div>
-      Hello
+    <div className='page' >
+      <Typography variant='h2' >BMI</Typography>
+      <Typography variant='h4' >Calculator</Typography>
+       <div className='input-section' >
+      <TextField variant='outlined' className='input-elem' color='secondary' label='Name'  />
+      <TextField variant='outlined' className='input-elem' color='secondary' label='CNIC' />
+      <div className='input-data' >
+      <TextField variant='outlined' type='number' className='input-elem-data'  color='secondary' label='Heigth (cm)' />
+      <TextField variant='outlined' type='number' className='input-elem-data' color='secondary' label='Weight (kg)' />
+      </div>
     </div>
+    <BMICard />
+    <BMICard />
+    <BMICard />
+    <BMICard />
+    <BMICard />
+    <BMICard />
+    </div>
+   
   );
 }
 
---
+++ src/Components/BMICard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Card.css'
+
+const BMICard = () => {
+    return (
+        <div className='card' >
+            <h1>Name</h1>
+            <p>CNIC</p>
+            <p>Height</p>
+            <p>Weight</p>
+            <p>BMI</p>
+            <p>Status</p>
+        </div>
+    )
+}
+
+export default BMICard
